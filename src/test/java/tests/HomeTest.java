d571cd714c437e98f180c7800cc800e5bcb4c97c
Agatha <auv2@cornell.edu>
2022-10-13 20:11:28 -0400

    first commit with framework

---
+++ src/test/java/tests/HomeTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class HomeTest extends BaseTest {
+
+    HomePage page;
+
+    @BeforeMethod
+    public void setUp() {
+
+        page = new HomePage(driver);
+    }
+}
