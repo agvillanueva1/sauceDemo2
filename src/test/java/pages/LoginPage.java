89d4747bf1a2ee84a60cc141f2c13f8f1bedd985
Agatha <auv2@cornell.edu>
2022-10-13 21:15:44 -0400

    working code for username and password and click login

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,6 +2,8 @@ package pages;
 
 import base.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
@@ -14,4 +16,14 @@ public class LoginPage extends BasePage {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+
+    @FindBy(id = "user-name")
+    public WebElement usernameField;
+
+    @FindBy(id = "password")
+    public WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    public WebElement loginBtn;
+
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -12,16 +12,21 @@ public class LoginTest extends BaseTest {
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
-        driver.findElement(By.xpath("//nav/a[text()='Calendar']")).click();
+        //driver.findElement(By.xpath("//nav/a[text()='Calendar']")).click();
         loginPage = new LoginPage(driver);
     }
 
     @Test(testName = "US301 Verify standard_user can login with right password")
     public void test01 (){
-        System.out.println("Test");
-    }
 
-    @Test(testName = "US302 ")
-    public void test02() {
+        //Input username
+        loginPage.usernameField.sendKeys("standard_user");
+
+        //Input password
+        loginPage.passwordField.sendKeys("secret_sauce");
+
+        //Click login
+        loginPage.loginBtn.click();
+
     }
 }
