f897346bb81d95c739ac3c3abd2bb8b52d02563e
Agatha <auv2@cornell.edu>
2022-10-13 23:39:44 -0400

    testing new line

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class LoginPage extends BasePage {
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver){
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+
+    @FindBy(id = "user-name")
+    public WebElement usernameField;
+
+    @FindBy(id = "password")
+    public WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    public WebElement loginBtn;
+    public WebElement thisIsTheTestThatINeed;
+}
