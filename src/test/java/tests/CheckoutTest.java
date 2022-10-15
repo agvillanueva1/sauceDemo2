c817981dfbd38445edd57e5e171eb2d57b38ad9d
ketarin phophrom <kphophrom@gmail.com>
2022-10-14 22:03:38 -0400

    Finished with CheckoutPage and CheckoutTest

--- src/test/java/tests/CheckoutTest.java
+++ src/test/java/tests/CheckoutTest.java
@@ -6,20 +6,36 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.CheckoutPage;
+import pages.LoginPage;
 
 public class CheckoutTest extends BaseTest {
 
     private CheckoutPage checkoutPage;
+    private LoginPage loginPage;
 
     @BeforeMethod(alwaysRun = true)
     public void setUp(){
-        //driver.findElement(By.xpath("//nav/a[text()='Calendar']")).click();
-        driver.get("https://www.saucedemo.com/checkout-step-one.html");
         checkoutPage = new CheckoutPage(driver);
+        loginPage = new LoginPage(driver);
     }
 
-    @Test(testName = "US 308 - Checkout: Your Information")
+    @Test(testName = "US 308 - Checkout: Your Information - Ketarin")
     public void test01(){
+
+        //Input username
+        loginPage.usernameField.sendKeys("standard_user");
+
+        //Input password
+        loginPage.passwordField.sendKeys("secret_sauce");
+
+        //Click login
+        loginPage.loginBtn.click();
+
+        //click cart
+        checkoutPage.cart.click();
+
+        //click checkout button
+        checkoutPage.checkoutBtn.click();
 
         //check first name field is displayed
         Assert.assertTrue(checkoutPage.firstNameField.isDisplayed());
@@ -31,8 +47,17 @@ public class CheckoutTest extends BaseTest {
         Assert.assertTrue(checkoutPage.zipcode.isDisplayed());
     }
 
-    @Test(testName = "US 309 - Add to cart")
+    @Test(testName = "US 309 - Add to cart - Ketarin")
     public void test02(){
+
+        //Input username
+        loginPage.usernameField.sendKeys("standard_user");
+
+        //Input password
+        loginPage.passwordField.sendKeys("secret_sauce");
+
+        //Click login
+        loginPage.loginBtn.click();
 
         //add bike light to cart
         checkoutPage.addBikeLight.click();
